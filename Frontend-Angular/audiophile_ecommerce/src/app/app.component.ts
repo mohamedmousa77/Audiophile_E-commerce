a430338339d27e8e6a6c67f1445c27535efde36e
Mohamed Mousa <77318165+mohamedmousa77@users.noreply.github.com>
2025-07-15 13:39:03 +0200

    Working on the home page

--- Frontend-Angular/audiophile_ecommerce/src/app/app.component.ts
+++ Frontend-Angular/audiophile_ecommerce/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { HeaderComponent } from './shared/components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
 
 HeaderComponent
 @Component({
@@ -9,7 +10,8 @@ HeaderComponent
   imports: [
     RouterOutlet,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'

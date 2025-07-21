3b795d32160701082e5e155e35d1dba015027986
Mohamed Mousa <77318165+mohamedmousa77@users.noreply.github.com>
2025-07-21 03:43:27 +0200

    Adjusted the footer. Working on product page

--- Frontend-Angular/audiophile_ecommerce/src/app/pages/home/promotions/promotions.component.ts
+++ Frontend-Angular/audiophile_ecommerce/src/app/pages/home/promotions/promotions.component.ts
@@ -7,26 +7,6 @@ import { CommonModule } from '@angular/common';
   styleUrl: './promotions.component.css'
 })
 export class PromotionsComponent {
-  zx9Product = {
-    title: 'ZX9 SPEAKER',
-    description: 'Upgrade to premium speakers that are phenomenally built to deliver truly remarkable sound.',
-    imageUrl: '3D_Speaker-image.png',
-    alt: 'ZX9 Speaker',
-    link: '/product/zx9'
-  };
-  zx7Product = {
-    title: 'ZX7 SPEAKER',
-    imageUrl: 'speacker-img.png',
-    link: '/product/zx7-speaker',
-    alt: 'ZX9 Speaker',
-  };
-
-  yx1Product = {
-    title: 'YX1 EARPHONES',
-    imageUrl: 'earphone-cat-img-bg.png',
-    link: '/product/yx1-earphones',
-    alt: 'ZX9 Speaker',
-  };
 
   promotionsProducts = [
     {

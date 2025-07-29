478174299d1f289870e972c1f41f331cfba5d128
Mohamed Mousa <77318165+mohamedmousa77@users.noreply.github.com>
2025-07-29 15:49:27 +0200

    Updating prduct details page. Working on cart logic

--- Frontend-Angular/audiophile_ecommerce/src/app/pages/product-details/product-details.component.ts
+++ Frontend-Angular/audiophile_ecommerce/src/app/pages/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ProductService } from '../../services/product/product.service';
 import { CommonModule } from '@angular/common';
+import { Product } from '../../models/product';
 
 @Component({
   selector: 'app-product-details',
@@ -44,38 +45,65 @@ export class ProductDetailsComponent {
     shortDescription: 'Studio quality headphones with noise cancelling.',
   }
 ];
-
+  productsSuggested: Product[] = [];
 
   constructor(
-    private route: ActivatedRoute, 
-    private router: Router, 
-    private productS: ProductService
+    private router: Router,
+    private productService: ProductService
   ) { }
 
   ngOnInit() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
+
     const navigation = this.router.getCurrentNavigation();
     this.product = navigation?.extras?.state?.['product'];
-    
-    console.log("Product received via router state:", this.product);
-    console.log("Product received via router state:", navigation?.extras?.state?.['product']);
-    
+
     const storedProduct = sessionStorage.getItem('selectedProduct');
     if (storedProduct) {
       this.product = JSON.parse(storedProduct);
     } else {
-      // fallback API call
+      // fallback API call if needed.
     }
-  console.log("Loaded product:", this.product);
-    
-    // if (!this.product) {
-    //   // fallback
-    //   this.productId = parseInt(this.route.snapshot.paramMap.get('id')!);
-    //   this.product = this.productS.getProductById(this.productId);
-    //   console.warn("Product not found in navigation state. ID from URL:", this.productId);
-    // } else {
-    //   console.log("Product received via router state:", this.product);
-    // }
+
+    if(this.product){
+      this.fetchSuggestedProducts(this.product.category);
+    }
   }
+
+  incrementQuantity(){
+    this.productQuantity++;
+  }
+  discrementQuantity(){
+    if(this.productQuantity <= 1)
+      {
+        null;
+      }
+      else{ 
+        this.productQuantity--;
+      } 
+  }
+
+  viewProduct(product: Product) {
+    
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+    sessionStorage.setItem('selectedProduct', JSON.stringify(product));    
+    this.router.navigate(['/product', product.id]);
+  }
+
+  fetchSuggestedProducts(categoryName: string): void {
+    console.log(`Fetch products called. Product category is: ${this.product.category}`);
+    this.productService.getProductsByCategory(categoryName)
+    .subscribe(products => {
+      this.productsSuggested = products;
+      this.productsSuggested.forEach(element => {
+        if(!element.imageUrl){
+          element.imageUrl = 'place-holder-3.png';
+        }
+      });
+      console.log(`Prodcuts fetched: ${this.productsSuggested}`);
+      
+    });      
+  }
+  
 
 }
